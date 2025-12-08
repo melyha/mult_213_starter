71d6de7e49f9dc7b96d7f203b95ec7c4bac4fc43
melxcreative <melyhaaly@gmail.com>
2025-12-08 13:23:37 -0600

    add Material UI icon and gradient styling to Header component

--- react_app/todo_app/src/Header.jsx
+++ react_app/todo_app/src/Header.jsx
@@ -1,12 +1,17 @@
 import './Header.css';
-
+import { Assignment, CheckCircle } from '@mui/icons-material';
 // Header component that displays title and message
 export function Header(props) {
     // props is some JSON that looks like {title: str, message: str}
     return (<>
         <header>
+            <div className="header-content">
+                <CheckCircle className="header-icon" />
+                <div>
             <h1>{props.title}</h1>
             <p>{props.message}</p>
+            </div>
+            </div>
         </header>
     </>);
 }
